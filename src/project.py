2baeda3337703cf9eef084093b42de45bd3790ca
Alyssa Vega <alyjvega@gmail.com>
2025-11-11 14:17:12 -0600

    added import and class Ball with init and update_surface methods

--- src/project.py
+++ src/project.py
@@ -1,2 +1,22 @@
+import random
+import pygame
 
+class Ball():
+
+    def __init__(self, pos, size, color, radius):
+        self.pos = pos
+        self.size = size
+        self.color = random.choice(color)
+        self.radius = radius
+
+        self.surface = self.update_surface()
+
+    def update_surface(self):
+        x, y, = self.pos
+        y += self.size
+        self.pos = (x,y)
+
+        surf = pygame.Surface((self.radius * 1.2, self.radius * 2), pygame.SRCALPHA)
+        pygame.draw.circle(surf, self.color, (x, y), self.size*0.8)
+        return surf
 
