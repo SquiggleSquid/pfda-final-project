fd36e4b6bf6b4733264fbe51141b8b941f304f42
Alyssa Vega <alyjvega@gmail.com>
2025-11-11 14:27:03 -0600

    added main function

--- src/project.py
+++ src/project.py
@@ -24,3 +24,44 @@ class Ball():
         self.surface = surface
         surface.blit(self.surface, self.pos)
 
+def main():
+    pygame.init()
+    pygame.display.set_caption("Digital Rain")
+    clock = pygame.time.Clock()
+    dt = 0
+    resolution = (800, 600)
+    screen = pygame.display.set_mode(resolution)
+
+    ball = Ball(resolution)
+
+    running = True
+    while running:
+        # Event Loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            # Mouse/keyboard controls for platform (racket)
+            #elif event.type == pygame.KEYDOWN:
+                #if event.key == pygame.K_1:
+                    #new_color = palette1
+                    #Particle((0,0), 15, 1000, palette1, 7.5)
+
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            running=False
+
+        # Game logic
+        ball.update(dt)
+        # Render & Display
+        black = pygame.Color (0,0,0)
+        screen.fill(black)
+        ball.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(12)
+pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
